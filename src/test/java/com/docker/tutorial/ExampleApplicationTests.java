fd2de5c4d070f7a9395c4136552165515c7a5c50
Vladimir Chisbora <vladimir_chisbora@smart-x.net>
2022-03-20 16:16:45 +0200

    - work in progress

--- src/test/java/com/docker/tutorial/ExampleApplicationTests.java
+++ src/test/java/com/docker/tutorial/ExampleApplicationTests.java
@@ -1,13 +1,19 @@
 package com.docker.tutorial;
 
+import lombok.AllArgsConstructor;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import java.util.HashMap;
+import java.util.Map;
+
+
+@AllArgsConstructor
 class ExampleApplicationTests {
 
-//	@Test
-//	void contextLoads() {
-//	}
-//
+    private Map<String, String> map = new HashMap<>();
+
+    @Test
+    public void test() {
+        System.out.println("here ");
+    }
 }
